10aec92026278ee06e17703fcafcd9ebb11ac6e1
Fedor Metsger <fedor.metsger@gmail.com>
2023-11-04 20:05:59 +0300

    Add celery support

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django_rest_passwordreset.tokens import get_token_generator
 
 # Create your models here.
 
